9e563d778a5324d6081e7577415dbc84d6cd1bcd
SuryawanshiRahul <suryawanshirahul9623@gmail.com>
2022-04-01 11:47:54 +0530

    MODIFIED: Roll Die Using Random Function 1 TO 6

--- src/com/infogalaxy/Snake_and_ladder.java
+++ src/com/infogalaxy/Snake_and_ladder.java
@@ -9,10 +9,15 @@ public class Snake_and_ladder {
         //UC-1 Game played with Single Player At Start Position is 0
         System.out.println("Player Position is :"+position);
     }
-
+    public void  rollsDie(){
+        //UC-2-Rolls_Die_1_To_6_Using_Random_Function
+        Random random = new Random();
+        die =random.nextInt(6)+1;
+        System.out.println("Die No is :");
+    }
     public static void main(String[] args) {
         Snake_and_ladder snake_and_ladder = new Snake_and_ladder();
         snake_and_ladder.showPosition();
-
+        snake_and_ladder.rollsDie();
     }
 }
