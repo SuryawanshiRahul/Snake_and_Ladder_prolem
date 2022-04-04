f40891ea74958fc0f893ebdd0c39ae0273ba8d70
SuryawanshiRahul <suryawanshirahul9623@gmail.com>
2022-04-04 10:41:56 +0530

    MODIFIED: Roll Die Using Random Function 1 TO 6 Die

--- src/com/infogalaxy/Snake_and_ladder.java
+++ src/com/infogalaxy/Snake_and_ladder.java
@@ -3,17 +3,25 @@ package com.infogalaxy;
 import java.util.Random;
 
 public class Snake_and_ladder {
+    Random random = new Random();
+
+    //Position of Player
     int position = 0;
-    int die;
+    //Define Constant For Options
+    final int NO_PLAY = 0;
+    final int LADDER = 1;
+    final int SNAKE = 2;
+
+    // Showing The Player Current Position
     public void showPosition(){
         //UC-1 Game played with Single Player At Start Position is 0
         System.out.println("Player Position is :"+position);
     }
+    // Rolling The Die to get  position
     public void  rollsDie(){
         //UC-2-Rolls_Die_1_To_6_Using_Random_Function
-        Random random = new Random();
-        die =random.nextInt(6)+1;
-        System.out.println("Die No is :");
+       int die =random.nextInt(6)+1;
+        System.out.println("Die No is :"+die);
     }
     public static void main(String[] args) {
         Snake_and_ladder snake_and_ladder = new Snake_and_ladder();
